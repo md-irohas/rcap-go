5fce112e88dd6cf4c90acb16d2208348542f75bc
md.irohas <md.irohas@gmail.com>
2023-07-01 19:35:46 +0900

    test: add test cases for reader.go

--- rcap/reader_test.go
+++ rcap/reader_test.go
@@ -2,41 +2,79 @@ package rcap
 
 import (
 	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/gopacket/layers"
+	"github.com/google/gopacket/pcap"
 )
 
-func TestReader(t *testing.T) {
-	// TODO
-	// Root required.
+func makeReader() *Reader {
+	reader := &Reader{}
+	reader.handle, _ = pcap.OpenOffline("testdata/sample.pcap")
+	return reader
+}
+
+func TestNewReader(t *testing.T) {
+	c := makeConfig()
+
+	if _, err := NewReader(c); err == nil {
+		t.Errorf("err is expected, but got '%v'.", err)
+	}
+}
+
+func TestReaderLinkType(t *testing.T) {
+	r := makeReader()
+	if r.LinkType() != layers.LinkTypeEthernet {
+		t.Errorf("'%v' is expected, but got '%v'.", layers.LinkTypeEthernet, r.LinkType())
+	}
 }
 
 func TestReaderNumPackets(t *testing.T) {
-	reader := &Reader{}
+	r := &Reader{}
 
-	if reader.NumPackets() != 0 {
-		t.Errorf("'%#v' is expected, but got '%#v'.", 0, reader.NumPackets())
+	if r.NumPackets() != 0 {
+		t.Errorf("'%#v' is expected, but got '%#v'.", 0, r.NumPackets())
 	}
 }
 
 func TestReaderResetNumPackets(t *testing.T) {
-	reader := &Reader{numPackets: 5}
-
-	if reader.NumPackets() != 5 {
-		t.Errorf("'%#v' is expected, but got '%#v'.", 5, reader.NumPackets())
+	r := &Reader{numPackets: 5}
+	if r.NumPackets() != 5 {
+		t.Errorf("'%#v' is expected, but got '%#v'.", 5, r.NumPackets())
 	}
 
-	reader.ResetNumPackets()
-
-	if reader.NumPackets() != 0 {
-		t.Errorf("'%#v' is expected, but got '%#v'.", 0, reader.NumPackets())
+	r.ResetNumPackets()
+	if r.NumPackets() != 0 {
+		t.Errorf("'%#v' is expected, but got '%#v'.", 0, r.NumPackets())
 	}
 }
 
-func TestReadPacket(t *testing.T) {
-	// TODO
-	// Root required.
+func TestReaderReadPacket(t *testing.T) {
+	r := makeReader()
+
+	expectedPayload := []byte("this is a test packet.\n")
+	expectedTime := int64(1688205150)
+	data, capinfo, err := r.ReadPacket() // first packet
+
+	// compare payloads only.
+	payload := data[len(data)-len(expectedPayload):]
+	if !cmp.Equal(payload, expectedPayload) {
+		t.Errorf("'%v' is expected, but got '%v'.", expectedPayload, payload)
+	}
+	if capinfo.Timestamp.Unix() != expectedTime {
+		t.Errorf("'%v' is expected, but got '%v'.", expectedTime, capinfo.Timestamp)
+	}
+	if err != nil {
+		t.Errorf("'%v' is expected, but got '%v'.", nil, err)
+	}
+
+	_, _, err = r.ReadPacket() // second packet (EOF)
+	if err == nil {
+		t.Errorf("err is expected, but got '%v'.", nil)
+	}
 }
 
 func TestReaderClose(t *testing.T) {
-	// TODO
-	// Root required.
+	r := makeReader()
+	r.Close()
 }
